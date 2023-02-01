94b5d16b176e750a69052f38ba6d0e29947f2fd6
Benjamin Mumbita <benjaminmumbita10@gmail.com>
2023-02-01 23:07:59 +0100

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      spacing:{
+        'sob':'-3rem',
+        'sub':'50%',
+      },
+    },
+  },
+  
+
+  plugins: [],
+}
